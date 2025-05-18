63f545a0ef43c2d250af31e04de57ff17f4b3672
Cedric Rabarijohn <cedrictianarabarijohn@gmail.com>
2025-05-18 03:07:03 +0300

    feat: add interfaces declarations

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,11 @@
 export interface IComponentWithChildren {
     children: React.ReactNode;
-}
+};
+
+export interface IUserDetails {
+    id: number;
+    email: string;
+    firstname: string;
+    lastname: string;
+    avatar: string;
+};
